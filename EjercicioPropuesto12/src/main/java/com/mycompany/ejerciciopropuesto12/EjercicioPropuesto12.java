336c6d77820bb353f8c12c68c3ba8d17d3c315c2
ElenaArangoDurango <earangod@unal.edu.co>
2025-09-06 13:18:41 -0500

    Initial commit

---
+++ EjercicioPropuesto12/src/main/java/com/mycompany/ejerciciopropuesto12/EjercicioPropuesto12.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciopropuesto12;
+
+/**
+ *
+ * @author arang
+ */
+public class EjercicioPropuesto12 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/poob/POOB.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poob;
+
+/**
+ *
+ * @author arang
+ */
+public class POOB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
+
+
