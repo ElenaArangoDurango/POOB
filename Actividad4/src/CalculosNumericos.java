ec6b9612bcc79bf98671840a33fe89cab63d5458
ElenaArangoDurango <earangod@unal.edu.co>
2025-10-31 07:36:32 -0500

    Ejercicio412 completo

--- Actividad4/src/CalculosNumericos.java
+++ Actividad4/src/CalculosNumericos.java
@@ -2,47 +2,46 @@ import java.util.InputMismatchException;
 
 public class CalculosNumericos {
 
-static void calcularLogaritmoNeperiano(double valor) {
-    try {
-        if (valor < 0) {
-            // Si el valor es negativo, se genera una excepción aritmética
-            throw new ArithmeticException("El valor debe ser un número positivo");
+    public static double calcularLogaritmoNeperiano(double valor) {
+        double resultado = 0;
+        try {
+            if (valor < 0) {
+                // Si el valor es negativo, se genera una excepción aritmética
+                throw new ArithmeticException("El valor debe ser un número positivo");
+            }
+
+            // Si el valor es positivo, se calcula el logaritmo neperiano
+            resultado = Math.log(valor);
+            return resultado;
+
+        } catch (ArithmeticException e) {
+            // Si ocurre una excepción aritmética, se muestra un mensaje de error
+            System.out.println("El valor debe ser un número positivo para calcular el logaritmo");
+            return resultado;
+
+        } catch (InputMismatchException e) {
+            // Si el valor a calcular no es numérico, se muestra un mensaje de error
+            System.out.println("El valor debe ser numérico para calcular el logaritmo");
+            return resultado;
         }
-
-        // Si el valor es positivo, se calcula el logaritmo neperiano
-        double resultado = Math.log(valor);
-        System.out.println("Resultado = " + resultado);
-
-    } catch (ArithmeticException e) {
-        /* Si ocurre una excepción aritmética, se muestra un mensaje de error */
-        System.out.println("El valor debe ser un número positivo para calcular el logaritmo");
-
-    } 
-    catch (InputMismatchException e) {
-        /* Si el valor a calcular no es numérico, se muestra un mensaje de error */
-        System.out.println("El valor debe ser numérico para calcular el logaritmo");
     }
-}
- static void calcularRaízCuadrada(double valor) {
- try {
-        if (valor < 0) {
-        // Si el valor es negativo, se genera una excepción aritmética
-        throw new ArithmeticException("El valor debe ser un número positivo");
+
+    public static double calcularRaizCuadrada(double valor) {
+        double resultado = 0;
+        try {
+            if (valor < 0) {
+                // Si el valor es negativo, se genera una excepción aritmética
+                throw new ArithmeticException("El valor debe ser un número positivo");
+            }
+            return Math.sqrt(valor);
+
+        } catch (ArithmeticException e) {
+            System.out.println("El valor debe ser un número positivo para calcular la raíz cuadrada");
+            return resultado;
+
+        } catch (InputMismatchException e) {
+            System.out.println("El valor debe ser numérico para calcular la raíz cuadrada");
+            return resultado;
         }
-        double resultado = Math.sqrt(valor); /* Si el valor es positivo, 
-        se calcula la raíz cuadrada */
-
-        System.out.println("Resultado = " + resultado);
-    } 
- catch (ArithmeticException e) {
- /* Si ocurre una excepción aritmética, se muestra un mensaje 
-de error */
-    System.out.println("El valor debe ser un número positivo para calcular la raíz cuadrada");
-    } 
-catch (InputMismatchException e) {
- /* Si el valor a calcular no es numérico, se muestra un mensaje 
-de error */
-    System.out.println("El valor debe ser numérico para calcular la raíz cuadrada" );
     }
- }
 }
